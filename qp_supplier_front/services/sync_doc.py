90c61485a34b84db045ef888ae9cf38b088e2f77
Rafael Licett <rafael.licett@mentum.group>
2025-04-14 18:10:18 +0000

    [add] sync supplier and purchase invoice

--- qp_supplier_front/services/sync_doc.py
+++ qp_supplier_front/services/sync_doc.py
@@ -3,7 +3,7 @@ import frappe
 from qp_authorization.use_case.bearer.authorize import send_request
 from qp_supplier_front.exception.sync import ExceptionSyncResponseEmpty, ExceptionSyncNoNewRecords, ExceptionSyncProductNotFound, ExceptionSyncRequestNotList, ExceptionSyncDocNotList, ExceptionSyncResponse
 
-def setup_doc(supplier_id, endpoint, request_key, request_key_id, request_list_key, request_list_key_id ,doctype, doctype_key, doctype_list_key, doctype_list, doctype_list_key_id, is_validate_items, get_doc_base ,set_item):
+def setup_doc(supplier_id, endpoint, request_key, request_key_id, request_list_key, request_list_key_id ,doctype, doctype_key, doctype_list_key, doctype_list, doctype_list_key_id, is_validate_items, get_doc_base ,set_item = None):
     
     message = None
     title = f"Error sync {doctype}"
@@ -46,7 +46,7 @@ def setup_doc(supplier_id, endpoint, request_key, request_key_id, request_list_k
                     
             frappe.log_error(message=message, title=title) 
         
-def set_doc(result, docs_new, request_key, request_key_id ,request_list_key, doctype, items_code, doctype_list_key, request_list_key_id, is_validate_items, get_doc_base, set_item):    
+def set_doc(result, docs_new, request_key, request_key_id ,request_list_key, doctype, items_code, doctype_list_key, request_list_key_id, is_validate_items, get_doc_base, set_item = None):    
     
     assertRequestValid(result, request_key)
     
@@ -108,72 +108,76 @@ def set_doc(result, docs_new, request_key, request_key_id ,request_list_key, doc
         
 def sync_item(doc, doc_new, items_code, set_item, doctype, doctype_list_key, request_list_key_id, request_list_key):
     
-    if not doc.qp_is_item_sync:
+    if set_item and not doc.qp_is_item_sync:
         
         frappe.enqueue(f"qp_supplier_front.services.background.set_item.handler", doc = doc, products=doc_new.get(request_list_key)[30:], items_valid = items_code, key_item =  doctype_list_key, key_id = request_list_key_id, set_item = set_item, queue='long', is_async=True, timeout=14400, job_name=f"send sync {doctype} doc {doc.name}")
                 
 def set_doc_control(doc, doc_new, request_list_key, doctype_list_key):
     
-    assertDocListNotEmpty(doc, doctype_list_key)
-    
-    doc.qp_item_sync = len(doc_new.get(request_list_key))
+    if request_list_key and doctype_list_key:
         
-    doc.qp_item_count = len(doc.get(doctype_list_key))
-    
-    doc.qp_is_item_sync = doc.qp_item_sync == doc.qp_item_count       
+        assertDocListNotEmpty(doc, doctype_list_key)
+        
+        doc.qp_item_sync = len(doc_new.get(request_list_key))
+            
+        doc.qp_item_count = len(doc.get(doctype_list_key))
+        
+        doc.qp_is_item_sync = doc.qp_item_sync == doc.qp_item_count       
                        
-def set_doc_list(doc, doc_new, doctype, request_list_key, items_code, request_list_key_id, doctype_list_key, request_key_id, is_validate_items, set_item):
+def set_doc_list(doc, doc_new, doctype, request_list_key, items_code, request_list_key_id, doctype_list_key, request_key_id, is_validate_items, set_item = None):
     
-    count = 0
-    
-    assertRequestListNotEmpty(doc_new.get(request_list_key), request_key_id)
-    
-    message=json.dumps(doc_new)
-    
-    for item in doc_new.get(request_list_key)[:30]:
+    if set_item:
         
-        title=f"Error sync {doctype}: {item.get(request_key_id)}"
+        count = 0
+        
+        assertRequestListNotEmpty(doc_new.get(request_list_key), request_key_id)
+        
+        message=json.dumps(doc_new)
+        
+        for item in doc_new.get(request_list_key)[:30]:
+            
+            title=f"Error sync {doctype}: {item.get(request_key_id)}"
 
-        try:
+            try:
+                
+                count += 1
+                
+                assertProductExist(item.get(request_list_key_id), items_code, is_validate_items)
+                
+                doc.append(doctype_list_key, set_item(item))
             
-            count += 1
-             
-            assertProductExist(item.get(request_list_key_id), items_code, is_validate_items)
+            except ExceptionSyncProductNotFound as e:  
+                title = str(e)
+                
+                doc.qp_is_error = True
+                
+                doc.append("lines_errors", {
+                    "line":count,
+                    "code": item.get(request_list_key_id),
+                    "error": title
+                })
+                message=json.dumps(item)
+                pass
             
-            doc.append(doctype_list_key, set_item(item))
-        
-        except ExceptionSyncProductNotFound as e:  
-            title = str(e)
-            
-            doc.qp_is_error = True
-            
-            doc.append("lines_errors", {
-                "line":count,
-                "code": item.get(request_list_key_id),
-                "error": title
-            })
-            message=json.dumps(item)
-            pass
-        
-        except Exception as e:
-            
-            doc.qp_is_error = True
-            
-            title = str(e)
-            
-            doc.append("lines_errors", {
-                "line":0,
-                "code": 0,
-                "error": title
-            })
-            message=json.dumps(item)
-            pass
-            
-        finally:            
-            
-            if doc.qp_is_error == True:
-            
-                frappe.log_error(message=message, title=title)
+            except Exception as e:
+                
+                doc.qp_is_error = True
+                
+                title = str(e)
+                
+                doc.append("lines_errors", {
+                    "line":0,
+                    "code": 0,
+                    "error": title
+                })
+                message=json.dumps(item)
+                pass
+                
+            finally:            
+                
+                if doc.qp_is_error == True:
+                
+                    frappe.log_error(message=message, title=title)
 
 def get_docs_new(result, request_key, request_key_id, doctype_key, doctype):
     
--- qp_supplier_front/uses_cases/supplier/approve.py
+++ qp_supplier_front/uses_cases/supplier/approve.py
@@ -1,7 +1,7 @@
 from qp_supplier_front.services.get_data import get_supplier
 APPROVE = "Aprobado"
 from qp_supplier_front.constant.endpoint import SUPPLIER_INSERT
-from qp_supplier_front.services.get_data import get_party, get_dynamic_link
+from qp_supplier_front.services.get_data import get_party, get_dynamic_link, get_bank_accounts
 from qp_authorization.use_case.bearer.authorize import send_request
 
 def handler(supplier_id):
@@ -24,16 +24,24 @@ def sync_supplier(supplier):
     
     addresses = get_dynamic_link(supplier, "Address")
     
+    bank_accounts = get_bank_accounts(supplier, "Bank Account")
+    
     contacts = get_dynamic_link(supplier, "Contact")
     
     payload ={
         "vendorId": supplier.name,
+        "firstName": "",
+        "middleName": "",
+        "firstSurname": "",
+        "secondSurname": "",
         "name": supplier.supplier_name,
+        "nit": "string",
         "documentType": party.id_type_id,
         "businessType": party.business_type,
         "phone": party.phone_number,
         "phone2": "",
         "phone3": "",
+        "landlinePhone": "string",
         "address": {
             "country": addresses[0].country,
             "state": addresses[0].state,
@@ -42,7 +50,14 @@ def sync_supplier(supplier):
         },
         "mail": contacts[0].user,
         "regime": party.tax_regime,
-        "ciiu": party.ciiu_id
+        "nature": 0,
+        "ciiu": party.ciiu_id,
+        "eftInformation": {
+            "bankName": "string",
+            "ibanCode": "string",
+            "swiftCode": "string",
+            "abaCode": "string"
+        }
     }
     
     result = send_request(SUPPLIER_INSERT, payload = payload)
