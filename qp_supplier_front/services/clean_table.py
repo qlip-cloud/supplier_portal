efbf5419eb8c05a6e6c5de2f1b8ede1ef1de2135
Rafael Licett <rafael.licettt@gmail.com>
2025-06-09 21:37:52 +0000

    [error] asign constant variable in sync

---
+++ qp_supplier_front/services/clean_table.py
@@ -0,0 +1,62 @@
+import frappe
+@frappe.whitelist()
+def all():
+    
+    order()
+    invoice()
+    receipt()
+    execute_truncate("`tabqp_SP_LineErrorSync`")
+    frappe.db.commit()
+    
+@frappe.whitelist()
+def order():
+    
+    execute_truncate("`tabPurchase Order`")
+    
+    execute_truncate("`tabPurchase Order Item`")
+    
+    execute_delete("Purchase Order")
+    
+    frappe.db.commit()
+
+@frappe.whitelist()
+def invoice():
+    
+    execute_truncate("`tabqp_SP_PurchaseInvoice`")
+
+    execute_delete("qp_SP_PurchaseInvoice")
+    
+    frappe.db.commit()
+
+@frappe.whitelist()
+def receipt():
+    
+    execute_truncate("`tabPurchase Receipt`")
+    
+    execute_truncate("`tabqp_SP_PurchaseReceiptItem`")
+    
+    execute_delete("Purchase Receipt")
+    
+    frappe.db.commit()
+
+def execute_truncate(table):
+    
+    assertIsAdministrator()
+    
+    sql = f"TRUNCATE TABLE {table}"
+    
+    frappe.db.sql(sql)
+    
+def execute_delete(parenttype):
+    
+    assertIsAdministrator()
+    
+    sql = f"delete from tabqp_SP_LineErrorSync where parenttype = '{parenttype}'"
+    
+    frappe.db.sql(sql)
+    
+def assertIsAdministrator():
+    
+    if frappe.session.user != "Administrator":
+        
+        frappe.throw("No tiene permiso para realizar esta acción")
