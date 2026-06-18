2989482131b9839d824fbb46cb1bec360dde373b
Rafael Licett <rafael.licett@mentum.group>
2026-06-18 14:52:43 +0000

    feat: restrict Supplier form editing to Administrator role via new client-side hook

--- qp_supplier_front/hooks.py
+++ qp_supplier_front/hooks.py
@@ -33,6 +33,9 @@ web_include_js = ["/assets/qp_supplier_front/js/qp_supplier_front.js", "/assets/
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+    "Supplier": "public/js/supplier.js"
+}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
