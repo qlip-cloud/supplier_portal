c3186882f3622dd26226a997da74e89e790fc2f5
Rafael Licett <rafael.licett@mentum.group>
2026-07-14 22:34:14 +0000

    feat: add qp_order_confirmation_no and qp_supplier_delivery_note assignments in set_name function

--- qp_supplier_front/uses_cases/purchase_order/__init__.py
+++ qp_supplier_front/uses_cases/purchase_order/__init__.py
@@ -4,3 +4,4 @@ import frappe
 def set_name(doc, method):
     if doc.is_new() and doc.order_confirmation_no:
         doc.name = doc.order_confirmation_no
+        doc.qp_order_confirmation_no = doc.order_confirmation_no
--- qp_supplier_front/uses_cases/receipts/__init__.py
+++ qp_supplier_front/uses_cases/receipts/__init__.py
@@ -4,3 +4,4 @@ import frappe
 def set_name(doc, method):
     if doc.is_new() and doc.supplier_delivery_note:
         doc.name = doc.supplier_delivery_note
+        doc.qp_supplier_delivery_note = doc.supplier_delivery_note
